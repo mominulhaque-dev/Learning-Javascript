a7b61b36e31fe472293a4a17730dc75348be5170
mominul <ulhouqemomi39@gmail.com>
2025-12-16 22:56:06 +0600

    Today I have learned what is variable

---
+++ variable.js
@@ -0,0 +1,20 @@
+/**
+ * variable hocce value store kore.eta kono function er moddhe use kora jay. variable er value change kora jay. variable er value read kora jay. variable er value delete kora jay.variable er value update kora jay. variable er value reset kora jay. variable er value clone kora jay. variable er value merge kora jay. variable er value compare kora jay. variable er value serialize kora jay. variable er value deserialize kora jay. variable er value validate kora jay. variable er value transform kora jay. variable er value log kora jay. variable er value watch kora jay. variable er value unwatch kora jay. variable er value subscribe kora jay. variable er value unsubscribe kora jay. variable er value notify kora jay. variable er value bind kora jay. variable er value unbind kora jay. variable er value freeze kora jay. variable er value unfreeze kora jay. variable er value lock kora jay. variable er value unlock kora jay. variable er value snapshot kora jay. variable er value restore kora jay. variable er value history track kora jay. variable er value history clear kora jay. variable er value event emit kora jay. variable er value event listen kora jay. variable er value event unlisten kora jay.
+ * eta hocce ekta variable handler. variable ke manage korar jonno use kora hoy.
+ * variable hocce ekta powerful tool ja variable er value ke efficiently manage korte help kore.
+ */
+
+ var names = ["abul", "babul", "cabul", "dabul", "ebul"];
+ var ages = [10, 20, 30, 40, 50];
+
+ console.log("Names:"+names);
+ console.log("Ages:"+ages);
+
+/**
+ * variable hocce value store kore.eta kono function er moddhe use kora jay. variable er value change kora jay. variable er value read kora jay. variable er value delete kora jay.variable er value update kora jay. variable er value reset kora jay. variable er value clone kora jay. variable er value merge kora jay. variable er value compare kora jay. variable er value serialize kora jay. variable er value deserialize kora jay. variable er value validate kora jay. variable er value transform kora jay. variable er value log kora jay. variable er value watch kora jay. variable er value unwatch kora jay. variable er value subscribe kora jay. variable er value unsubscribe kora jay. variable er value notify kora jay. variable er value bind kora jay. variable er value unbind kora jay. variable er value freeze kora jay. variable er value unfreeze kora jay. variable
+ * 
+ */
+
+// vairable hocce kunu kicu store kore raka jonno. eta ekta container er moto kaj kore. variable er maddhome amra data ke easily manage korte pari. variable er value ke change, read, delete, update, reset, clone, merge, compare, serialize, deserialize, validate, transform, log, watch, unwatch, subscribe, unsubscribe, notify, bind, unbind, freeze, unfreeze kora jay. variable er maddhome amra data ke efficiently handle korte pari. variable er value ke snapshot nite pari ebong restore korte pari. variable er value er history track korte pari ebong clear korte pari. variable er value er event emit korte pari ebong listen korte pari. variable er value er event unlisten korte pari. variable hocce ekta powerful tool ja amader coding experience ke aro better kore tole.
+
+//
