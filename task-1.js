e82a33d8105816b0c1f09a6caf115cde9920d38c
mominul <ulhouqemomi39@gmail.com>
2026-01-28 22:55:13 +0600

    finished 1st task

---
+++ task-1.js
@@ -0,0 +1,23 @@
+var myHandcase = 1000;
+var orange = 450;
+var apple = 320;
+
+console.log("My handcase has = " + myHandcase + "tk");
+
+var totalCost = orange + apple;
+console.log("Total cost of orange and apple is = " + totalCost + "tk");
+
+var myRemainingBalance = myHandcase - totalCost;
+console.log("My balance after shopping = " + myRemainingBalance + "tk");
+
+// end of the code
+// I am finding more shortcut to write code.
+
+var orangePrice =455;
+var applePrice = 245;
+var myBalance =3532;
+ 
+var totalShoppingCost = orangePrice + applePrice ;
+console.log("totalCost"+ totalShoppingCost);
+
+console.log("My total balance after shopping = " + (myBalance - (orangePrice + applePrice)) + "tk");
---
+++ type.js
@@ -0,0 +1,7 @@
+// String type variable
+
+var name ="Mominul Hoque";
+
+var country= "Bangladesh";
+
+console.log("Name:"+ name, "Country:" + country)
