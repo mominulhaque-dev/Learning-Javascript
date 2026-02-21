15e94234547a3a74ec6c65b45be4803e0929f942
mominul <ulhouqemomi39@gmail.com>
2026-02-21 08:45:06 +0600

    pratics if else coditions

---
+++ If-else/if.js
@@ -0,0 +1,41 @@
+/**
+ * tui joidi amake khawas
+ * taile ami asob
+ *
+ * if you buy food for me
+ *
+ *      i will come
+ *
+ * Structure
+ *
+ *  if(condition){
+ *
+ * }
+ * else{
+ *
+ * }
+ *
+ * **/
+
+// if condition check
+
+// if(30<60){
+//     console.log("the 60 is larger");
+
+// }
+
+// If else conditions
+
+let age = 50;
+
+if (age > 51) {
+  console.log("He will enter the park");
+} else {
+  console.log("He have not persimion");
+}
+
+if (age <= 51) {
+  console.log("He will get permit");
+} else {
+  console.log("He will not get permit");
+}
