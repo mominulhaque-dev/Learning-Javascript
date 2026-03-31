1b4c446b1f57b4b87523cf206460a84122c298f4
mominul <ulhouqemomi39@gmail.com>
2026-03-31 17:16:52 +0600

    Practice nested if else

---
+++ nested.js
@@ -0,0 +1,23 @@
+// Nested  if - else statement
+
+const money = 1;
+
+if (money > 500) {
+  console.log(" bro You are very rich");
+} else {
+  if (money > 200) {
+    console.log("You are the middle person");
+  } else {
+    if (money > 100) {
+      console.log("You are the poor person");
+    } else {
+      console.log("You are the very poor person");
+    }
+  }
+}
+
+/* 
+ ekane nested if else statement use kora hoyese
+ mane if er modde if else statement use kora hoyese
+ 
+*/
